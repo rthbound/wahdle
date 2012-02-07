f66416cf1cd202d998b84fb90d79db8532947b99
Josh m <yoshie@nodomain.com>
2012-02-06 21:04:33 -0600

    removed yaml

--- config/initializers/suitcase.rb
+++ config/initializers/suitcase.rb
@@ -1,5 +1,2 @@
-SUITCASE_CONFIG = YAML.load_file("#{Rails.root}/config/suitcase_config.yml")[Rails.env]
-
-
-Suitcase::Configuration.hotel_api_key =SUITCASE_CONFIG['ean_hotel_ck']    
+Suitcase::Configuration.hotel_api_key =""
 Suitcase::Configuration.cache = Hash.new # set the caching mechanism (see below)
