81c2622b44dda6ab72a6cb1bc8754ff0f41e959c
Dave Levine <dmlevi@gmail.com>
2012-02-06 21:26:36 -0600

    added secret api key

--- config/initializers/suitcase.rb
+++ config/initializers/suitcase.rb
@@ -1,2 +1,2 @@
-Suitcase::Configuration.hotel_api_key =""
+Suitcase::Configuration.hotel_api_key ="xeutg54a3r93aetdwg94zz6c"
 Suitcase::Configuration.cache = Hash.new # set the caching mechanism (see below)
